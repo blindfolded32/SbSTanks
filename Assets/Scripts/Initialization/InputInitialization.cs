e937d9eaf76863d93314e6a773fa5fb56acb31f6
Artem <artem191020001@gmail.com>
2021-10-27 16:07:48 +0300

    Добавил инициализатор инпута

---
+++ Assets/Scripts/Initialization/InputInitialization.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputInitialization
+{
+    public event Action<bool> OnSpaceChange;
+
+    public void GetAxis()
+    {
+        OnSpaceChange?.Invoke(Input.GetKeyDown(KeyCode.Space));
+    }
+}

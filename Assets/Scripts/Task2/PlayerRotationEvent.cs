dda7a29944d939aac361177f54792ffdd0788e6f
Artem <artem191020001@gmail.com>
2021-11-01 02:43:34 +0300

    Попытка плавного поворота(неудачна)

--- Assets/Scripts/Task2/PlayerRotationEvent.cs
+++ Assets/Scripts/Task2/PlayerRotationEvent.cs
@@ -10,6 +10,11 @@ namespace SbSTanks
         private Transform _player;
         private Transform _enemyPosition;
 
+        private float _time = 20f;
+        private bool _isComplete;
+        private float _lerpProgress = 0;
+        private Quaternion _startRotation;
+
         public PlayerRotationEvent(PlayerModel playerModel, Transform enemyPosition, Button button)
         {
             _player = playerModel.GetPlayer.transform;
@@ -21,7 +26,19 @@ namespace SbSTanks
 
         public void RotatePlayerTank()
         {
-           _player.LookAt(_enemyPosition, Vector3.up);
+            //_startRotation = _player.rotation;
+            //while (_isComplete == false)
+            //{
+            //    _lerpProgress += Time.deltaTime / _time;
+            //    _player.rotation = Quaternion.Lerp(_startRotation, _enemyPosition.rotation, _lerpProgress);
+
+            //    if (_lerpProgress >= 1)
+            //    {
+            //        _isComplete = true;
+            //    }
+            //}
+            //_isComplete = false;
+            _player.LookAt(_enemyPosition, Vector3.up);
         }
 
     }

46c677e5863cbca5a04115d3c04c30c66e6db85b
vic-meateater <69753569+vic-meateater@users.noreply.github.com>
2021-12-02 16:34:33 +0500

    GameOverCanvas.cs

--- Assets/Scripts/Markers/RoundCanvas.cs
+++ Assets/Scripts/Markers/RoundCanvas.cs
@@ -2,13 +2,18 @@ using Controllers;
 using TMPro;
 using UnityEngine;
 
+
 namespace Markers
 {
     public class RoundCanvas : MonoBehaviour
     {
         private Animator _animator;
         private TextMeshProUGUI _textMeshPro;
+        private GameOverCanvas _gameOverCanvas;
         private static StepController _stepController;
+        private static readonly int NewRound = Animator.StringToHash("NewRound");
+
+        private const int GAME_PAUSE = 0;
 
         public static void Init(StepController stepController)
         {
@@ -21,23 +26,27 @@ namespace Markers
             _stepController.NewRound += ShowText;
             _stepController.NewTurn += ShowTurn;
             _stepController.ReInitController.GameOver += LostBaner;
+            _gameOverCanvas = FindObjectOfType<GameOverCanvas>();
+
         }
         private void ShowText(int roundNumber)
         {
             _textMeshPro.text = $"Round {roundNumber}";
-            _animator.Play("NewRound",-1,0f);
+            _animator.Play(NewRound,-1,0f);
         }
 
         private void ShowTurn(int turnNumber)
         {
             _textMeshPro.text = $"turn {turnNumber}";
-            _animator.Play("NewRound",-1,0f);
+            _animator.Play(NewRound,-1,0f);
         }
 
         private void LostBaner(int round)
         {
+            Time.timeScale = GAME_PAUSE;
             _textMeshPro.text = $"You reached round {round}. Try Better";
-            _animator.Play("NewRound",-1,0f);
+            _animator.Play(NewRound,-1,0f);
+            _gameOverCanvas.GetComponent<Canvas>().enabled = true;
         }
         
     }
